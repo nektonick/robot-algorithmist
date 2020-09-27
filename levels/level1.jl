5e1babd08897ae5efad5699a108160158ae014a8
nektonick <nik-1905@mail.ru>
2020-09-27 20:30:11 +0300

    Для level1.jl добавлен случай, когда изначально в некоторых клетках поля могут находиться маркеры.

--- levels/level1.jl
+++ levels/level1.jl
@@ -2,9 +2,36 @@ function level1(r::Robot)
     println("level1")
     r = Robot(; animate=true)
 
-    for side in (DirectionsOfMovement(i) for i=0:3) # - перебор всех возможных направлений
-        putmarkers!(r,side)
-        move_by_markers(r, inverse(side))
+    println("Есть ли в некоторых клетках поля маркеры? y/n")
+
+    userAnswer = readline()
+
+    
+
+    if (userAnswer == "n")
+        for side in (DirectionsOfMovement(i) for i=0:3)
+            putmarkers!(r,side)
+            move_by_markers(r, inverse(side))
+        end
+        putmarker!(r)
+    elseif (userAnswer == "y")
+        for side in (DirectionsOfMovement(i) for i=0:3)
+            steps = 0
+            while isborder(r,side)==false
+                move!(r,side)
+                if (!(ismarker(r)))
+                    putmarker!(r)
+                end
+                steps+=1
+            end
+            moves!(r, inverse(side), steps)
+        end
+        if (!(ismarker(r)))
+            putmarker!(r)
+        end
+    else
+        println("Неверный ввод. Завершение программы.")
     end
-    putmarker!(r)
+    
+
 end
