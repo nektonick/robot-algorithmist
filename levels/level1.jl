696620d6d0bf72ae11faa3e27027dce3dddbd736
nektonick <nik-1905@mail.ru>
2020-09-27 20:39:42 +0300

    В файле level1.jl простое и сложное решение выделены в отдельные функции.

--- levels/level1.jl
+++ levels/level1.jl
@@ -1,37 +1,40 @@
+function simpleSolution(r::Robot)
+    for side in (DirectionsOfMovement(i) for i=0:3)
+        putmarkers!(r,side)
+        move_by_markers(r, inverse(side))
+    end
+    putmarker!(r)
+end
+
+function difficultSolution(r::Robot)
+    for side in (DirectionsOfMovement(i) for i=0:3)
+        steps = 0
+        while isborder(r,side)==false
+            move!(r,side)
+            if (!(ismarker(r)))
+                putmarker!(r)
+            end
+            steps+=1
+        end
+        moves!(r, inverse(side), steps)
+    end
+    if (!(ismarker(r)))
+        putmarker!(r)
+    end
+end
+
 function level1(r::Robot)
     println("level1")
     r = Robot(; animate=true)
 
     println("Есть ли в некоторых клетках поля маркеры? y/n")
-
     userAnswer = readline()
 
-    
-
     if (userAnswer == "n")
-        for side in (DirectionsOfMovement(i) for i=0:3)
-            putmarkers!(r,side)
-            move_by_markers(r, inverse(side))
-        end
-        putmarker!(r)
+        simpleSolution(r)
     elseif (userAnswer == "y")
-        for side in (DirectionsOfMovement(i) for i=0:3)
-            steps = 0
-            while isborder(r,side)==false
-                move!(r,side)
-                if (!(ismarker(r)))
-                    putmarker!(r)
-                end
-                steps+=1
-            end
-            moves!(r, inverse(side), steps)
-        end
-        if (!(ismarker(r)))
-            putmarker!(r)
-        end
+        difficultSolution(r)
     else
         println("Неверный ввод. Завершение программы.")
     end
-    
-
 end
