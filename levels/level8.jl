7fbc2cb8cb924ac954b6d9dc610c0d8bd9dacae7
nektonick <nik-1905@mail.ru>
2020-10-08 02:54:10 +0300

    Добавлено решение для level8.jl

--- levels/level8.jl
+++ levels/level8.jl
@@ -1,4 +1,15 @@
 function level8(r)
     println("level8")
-    show(r)
+    println("Робот слева или справа от перегородки? L/R")
+    n = readline()
+    (n == "L") ? r=Robot("levels/situation_8_1.sit"; animate=true) : r=Robot("levels/situation_8_2.sit"; animate=true)
+    
+
+    n=0; side=Left
+    while isborder(r,Up)==true # прохода сверху нет
+        n+=1
+        moves!(r,side,n)
+        side=inverse(side)
+    end
+
 end
