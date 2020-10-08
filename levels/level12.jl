377543f4b2bc82b967fbc5b5dac4bf6ef3e86399
nektonick <nik-1905@mail.ru>
2020-10-08 11:05:02 +0300

    Добавлено решение для level12.jl

---
+++ levels/level12.jl
@@ -0,0 +1,56 @@
+function level12(r)
+    println("level12")
+    r=Robot(; animate=true)
+    println("Введите размер клтеки")
+    cellOfField = parse(Int, readline())
+    x=0; y=0
+    num_steps= []
+
+    while (isborder(r,Down)==false || isborder(r,Left)==false)
+        if (isborder(r, Down) == false)
+            push!(num_steps, "Up")
+            move!(r, Down)
+        end
+        if (isborder(r, Left) == false)
+            push!(num_steps, "Right")
+            move!(r, Left)
+        end
+    end
+
+    horisontalDirection = Right
+    while !(isborder(r, Up) && isborder(r, Right))
+        if(mod(x, 2*cellOfField)) < cellOfField && (mod(y, 2*cellOfField)) < cellOfField || 
+            (mod(x+ cellOfField, 2*cellOfField)) < cellOfField && (mod(y, 2*cellOfField)) >= cellOfField
+            putmarker!(r)
+        end
+        
+
+        if isborder(r, Right) || isborder(r, Left) && !(isborder(r, Down) && isborder(r, Left))
+            move!(r, Up)
+            y+=1
+            if (mod(x, 2*cellOfField)) < cellOfField && (mod(y, 2*cellOfField)) < cellOfField || 
+                (mod(x+ cellOfField, 2*cellOfField)) < cellOfField && (mod(y, 2*cellOfField)) >= cellOfField
+                putmarker!(r)
+            end
+            horisontalDirection = inverse(horisontalDirection)
+        end
+
+        move!(r,horisontalDirection)
+        (horisontalDirection == Right) ? x+=1 : x-=1
+    end
+    if(mod(x, 2*cellOfField)) < cellOfField && (mod(y, 2*cellOfField)) < cellOfField || 
+        (mod(x+ cellOfField, 2*cellOfField)) < cellOfField && (mod(y, 2*cellOfField)) >= cellOfField
+        putmarker!(r)
+    end
+    
+    moveToLeftDownCorner!(r)
+
+    for element in reverse(num_steps)
+        if (element == "Up")
+            move!(r, Up)
+        elseif (element == "Right")
+            move!(r, Right)
+        end
+    end
+
+end
